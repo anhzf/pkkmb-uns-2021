cbe64d4701da2ed475ace30a72c2c1c8442dc6ed
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-07-06 10:53:03 +0700

    style: :wrench: setup tailwindcss and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'next',
+    'next/core-web-vitals',
+    // 'plugin:@next/next/recomended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
