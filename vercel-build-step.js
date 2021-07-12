e2d67d85f2949bbc034ab3cbf9a38d4734ccede8
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-07-13 06:58:38 +0700

    ci: :construction_worker: add vercel build command

---
+++ vercel-build-step.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+/* eslint-disable no-console */
+
+console.log(`commit message: ${process.env.VERCEL_GIT_COMMIT_MESSAGE}`);
+
+// if the commit message contains '[ready]'
+if (/\[ready\]/ig.test(process.env.VERCEL_GIT_COMMIT_MESSAGE)) {
+  console.log('✅ - Build can proceed');
+  process.exit(1);
+} else {
+  console.log('🛑 - Build cancelled');
+  process.exit(0);
+}
